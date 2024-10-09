1d93e36528de7871a0fc72cc5a24657c25225286
Rodrigo Bruner <me@bruner.app>
2024-10-09 12:13:10 -0700

    feat:test for a valid equilateral triangle

---
+++ TriangleTest/TriangleUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace TriangleTest
+{
+    [TestFixture]
+    internal class TriangleUnitTest
+    {
+
+        [Test]
+        public void ValidEquilateralTriangle_Input60and60and60_OutputEquilateralTriangle()
+        {
+            // Arrange
+            int firstSide = 60;
+            int secondSide = 60;
+            int thirdSide = 60;
+
+            string expected = "Equilateral triangle";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+    }
+}
+
