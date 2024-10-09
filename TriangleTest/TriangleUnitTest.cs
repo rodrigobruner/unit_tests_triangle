36fbcd28a0645347ea99cd095c4e585bb0cf9aa8
Rodrigo Bruner <me@bruner.app>
2024-10-09 12:19:35 -0700

    feat: Check invalid scenarios

--- TriangleTest/TriangleUnitTest.cs
+++ TriangleTest/TriangleUnitTest.cs
@@ -168,6 +168,109 @@ namespace TriangleTest
         }
 
 
+        [Test]
+        public void InvalidTriangleZeroLength_Input0and30and30_OutputInvalidTriangle()
+        {
+            // Arrange
+            int firstSide = 0;
+            int secondSide = 30;
+            int thirdSide = 30;
+
+            string expected = "Invalid Triangle - a zero has been detected";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InvalidTriangleZeroLength_Input30and0and30_OutputInvalidTriangle()
+        {
+            // Arrange
+            int firstSide = 30;
+            int secondSide = 0;
+            int thirdSide = 30;
+
+            string expected = "Invalid Triangle - a zero has been detected";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InvalidTriangleZeroLength_Input30and30and0_OutputInvalidTriangle()
+        {
+            // Arrange
+            int firstSide = 30;
+            int secondSide = 30;
+            int thirdSide = 0;
+
+            string expected = "Invalid Triangle - a zero has been detected";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void InvalidTriangle_Input10and20and30_OutputInvalidTriangle()
+        {
+            // Arrange
+            int firstSide = 10;
+            int secondSide = 20;
+            int thirdSide = 30;
+
+            string expected = "INVALID!!";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InvalidTriangle_Input10and40and50_OutputInvalidTriangle()
+        {
+            // Arrange
+            int firstSide = 10;
+            int secondSide = 40;
+            int thirdSide = 50;
+
+            string expected = "INVALID!!";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InvalidTriangle_Input50and10and10_OutputInvalidTriangle()
+        {
+            // Arrange
+            int firstSide = 50;
+            int secondSide = 10;
+            int thirdSide = 10;
+
+            string expected = "INVALID!!";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+
     }
 }
 
